3bb5a476196bd4fb8868801d1a900833cd2da427
TehamiKamdar <tehamikamdar15@gmail.com>
2025-10-14 11:49:11 +0500

    Views added and tested

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
 
 const logRequest = (req, res, next) =>{
   console.log(`Received a ${req.method} request from ${req.ip}`);
@@ -9,8 +13,19 @@ const logRequest = (req, res, next) =>{
 app.use(logRequest)
 
 app.get('/', (req, res) => {
-  res.send("Hello World!")
+  const data = {
+    title: "Express Home",
+    heading: "Welcome to Express Home."
+  }
+  res.render('index', { data })
 });
 
+app.get('/about', (req, res) => {
+  const data = {
+    title: "Express About",
+    heading: "Welcome to Express About."
+  }
+  res.render('index', { data })
+});
 
 app.listen(3000, () => { console.log(`Server is running on http://localhost:3000`) })
